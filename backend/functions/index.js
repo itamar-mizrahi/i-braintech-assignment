78fbe7bf57641ac0547c50779e0f3e83d33d60f3
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2023-03-13 04:42:43 +0200

    feat: backend deployment , issue -payment

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,66 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+// API endpoint to create a new user
+exports.createUser = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    try {
+      const userRef = await db.collection('users').add(req.body);
+      res.status(201).send(userRef.id);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error creating user');
+    }
+  });
+});
+
+// API endpoint to add a new workout to a user's profile
+exports.addWorkout = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    try {
+      const workoutRef = await db.collection('workouts').add({
+        userId: req.params.userId,
+        date: req.body.date,
+        time: req.body.time,
+        type: req.body.type,
+        duration: req.body.duration,
+        caloriesBurned: req.body.caloriesBurned
+      });
+
+      await db.collection('users').doc(req.params.userId).update({
+        workoutIds: admin.firestore.FieldValue.arrayUnion(workoutRef.id)
+      });
+
+      res.status(201).send(workoutRef.id);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error adding workout');
+    }
+  });
+});
+
+// API endpoint to retrieve all workouts for a user
+exports.getWorkouts = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    try {
+      const workoutIds = (await db.collection('users').doc(req.params.userId).get()).data().workoutIds;
+
+      const workouts = [];
+
+      for (const id of workoutIds) {
+        const workout = (await db.collection('workouts').doc(id).get()).data();
+        workouts.push(workout);
+      }
+
+      res.status(200).send(workouts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving workouts');
+    }
+  });
+});
--- backend/index.js
+++ backend/index.js
@@ -71,28 +71,3 @@ app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: 'https://your-project-name.firebaseio.com'
-// });
-
-// // Create a reference to the workout collection
-// const workoutsRef = admin.firestore().collection('workouts');
-
-// // Define a route to create a new workout
-// app.post('/workouts', async (req, res) => {
-//   const { userId, date, type, duration, caloriesBurned } = req.body;
-
-//   // Create a new document in the workouts collection with the provided data
-//   const newWorkoutRef = workoutsRef.doc();
-//   const newWorkout = {
-//     userId,
-//     date,
-//     type,
-//     duration,
-//     caloriesBurned
-//   };
-//   await newWorkoutRef.set(newWorkout);
-
-//   res.json({ id: newWorkoutRef.id });
-// });

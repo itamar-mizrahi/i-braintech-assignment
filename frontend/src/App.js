4461bcb7b48f3ac2a89d380261e0cf7b60a195bc
Itamar Mizrahi <theitamarmizrahi@gmail.com>
2024-06-12 23:45:23 +0300

    added AI comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import SignOut from "./components/SignOut";
 // import WorkoutList from ".//components/WorkoutList";
 import Dashboard from "./components/Dashboard";
+import AI from "./components/AI";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -25,10 +26,17 @@ function App() {
           <h1>Fitness Tracking App</h1>
 
           <LoginCheck isLoggedIn={isLoggedIn} />
+          <AI/>
           <iframe
             src="https://copilotstudio.microsoft.com/environments/Default-3c678821-7750-47a3-937f-2661439abb7a/bots/cr971_personalTrainer/webchat?__version__=2"
             title="Fitness Tracking App Chat"
-            style={{ width: "100%", height: "100%" }}
+            style={{
+              width: "100%",
+              height: "500px", // Increase the height to make it bigger
+              border: "none",
+              boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+              borderRadius: "5px",
+            }}
           ></iframe>
         </div>
       </header>
